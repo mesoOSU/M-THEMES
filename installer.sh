850b44f38f2feae7e3d16a2247ad62e6b94a0153
argerlt <argerlt@gmail.com>
2021-01-26 00:12:57 -0500

    cleanup auto-compiler and add testing file

---
+++ installer.sh
@@ -0,0 +1,19 @@
+BASE_DIR=$(pwd)
+cd $BASE_DIR/mpich/src
+rm -r mpich-3.2.1
+tar xf mpich-3.2.1.tar.gz
+cd $BASE_DIR/mpich/src/mpich-3.2.1
+./configure --prefix=$BASE_DIR/mpich
+make
+make install
+cd $BASE_DIR/fftw/src
+rm -r fftw-2.1.5
+tar xf fftw-2.1.5.tar.gz
+cd $BASE_DIR/fftw/src/fftw-2.1.5
+./configure --prefix=$BASE_DIR/fftw --enable-mpi MPICC=$BASE_DIR/mpich/bin/mpicc
+make
+make install
+cd $BASE_DIR/src
+rm *.o
+make
+cd $BASE_DIR
---
+++ test.sh
@@ -0,0 +1,3 @@
+cp src/forge test_run/
+cd test_run
+mpiexec -np 4 forge input.in
