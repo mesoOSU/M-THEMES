797111e669e4445fd0fb2ea0ade11d1aa27b97ce
Andrew Kingery <kandrew2022@gmail.com>
2021-09-02 13:03:23 -0400

    Add Parallel HDF Installation Support
    Added HDF5 source code to repo and modified the `installer.sh` script to use that
    and install hdf5 with paralell support locally inside the repository. Modified
    the Makefile to use the locally installed version of hdf5 instead of looking for
    a global one. This should make installation of M-THEMES easier.

--- installer.sh
+++ installer.sh
@@ -1,4 +1,5 @@
 BASE_DIR=$(pwd)
+# install mpich
 cd $BASE_DIR/mpich/src
 rm -r mpich-3.2.1
 tar xf mpich-3.2.1.tar.gz
@@ -6,6 +7,8 @@ cd $BASE_DIR/mpich/src/mpich-3.2.1
 ./configure --prefix=$BASE_DIR/mpich
 make
 make install
+
+# install fftw
 cd $BASE_DIR/fftw/src
 rm -r fftw-2.1.5
 tar xf fftw-2.1.5.tar.gz
@@ -13,6 +16,17 @@ cd $BASE_DIR/fftw/src/fftw-2.1.5
 ./configure --prefix=$BASE_DIR/fftw --enable-mpi MPICC=$BASE_DIR/mpich/bin/mpicc
 make
 make install
+
+# install HDF
+cd $BASE_DIR/hdf/src
+rm -r "hdf5-1.10.5"
+tar xf "hdf5-1.10.5.tar.gz"
+cd "$BASE_DIR/hdf/src/hdf5-1.10.5"
+./configure --enable-parallel --disable-fortran --prefix=$BASE_DIR/hdf
+make
+make install
+
+# build M-THEMES
 cd $BASE_DIR/src
 rm *.o
 make
